7fe3fa1a0fc2d53e398eb6f1a2b692dae2b34d83
HarishVidya <vee.harishkrishna@gmail.com>
2023-01-17 22:25:08 -0500

    added profile and profile functions

--- twitchclip/clipshare/views.py
+++ twitchclip/clipshare/views.py
@@ -3,23 +3,6 @@ from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Harish',
-        'title': 'Insane Trickshot',
-        'content': '360 no scope',
-        'date_posted': 'January 4, 2023',
-        'clip_link': 'ssdfkjsdfsd'
-    },
-    {
-        'author': 'John',
-        'title': 'Fortnite Clip',
-        'content': '1v4 clutch',
-        'date_posted': 'December 12, 2022',
-        'clip_link': 'ssdfkjsdfsd'
-    }
-]
-
 
 def home(request):
     context = {
